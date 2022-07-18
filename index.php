43860efbf73be50bd066f55e759644f47cbb8e57
Sujata Kushwaha <109337657+sujata9001@users.noreply.github.com>
2022-07-19 01:18:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 	<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	<style>
+<!-- 	<style>
 		body{
 			background: lightblue;
 		}
-	</style>
+	</style> -->
 	</head>
-	<body>
+	<body class="bg-primary">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6">
